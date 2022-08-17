e34b4727938d473152c4c6220e87a3681f62bac8
iguoliveira <igor.or042@gmail.com>
2022-08-17 16:57:13 -0300

    Update card

--- src/components/homepage/card/Card.jsx
+++ src/components/homepage/card/Card.jsx
@@ -1,7 +1,7 @@
 export const Card = () => {
     return(
-        <div>
-            <div>Muito mais que um banco, uma nação!</div>
+        <div className="flex flex-col items-center">
+            <div className="text-4xl font-bold">Muito mais que um banco, uma nação!</div>
             <div>
                 Rapido, seguro e agradavel!
             </div>
