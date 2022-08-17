ee7d91091e61bed3e43de05116ddf0be76f3d937
iguoliveira <igor.or042@gmail.com>
2022-08-17 11:34:11 -0300

    Changed brand logo + Changed link color

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,17 @@
-import BlackLogo from '/cat-logo-black.png'
+import Logo from '/cat-logo-colored.png'
+import { Links } from './links/Links';
 
 export const Navbar = () => {
   return (
     <div className="bg-white fixed inset-0 p-3">
-      <div className="flex justify-between mx-4">
-        <div className="flex gap-8 text-xl">
-          <img src={BlackLogo} className="w-16" />
+      <div className="flex justify-between items-center mx-4">
+        <div className="flex gap-8 text-lg">
+          <img src={Logo} className="w-16" />
 
-          <div className="flex items-center gap-3">
-            <div>SIM</div>
-            <div>OPA</div>
-            <div>DUDU</div>
+          <div className="flex items-center gap-5">
+            <Links name="Homepage" />
+            <Links name="About" />
+            <Links name="Our Benefits" />
           </div>
         </div>
 
---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,5 @@
+export const Links = (props) => {
+    return(
+        <div className="cursor-pointer text-mainBrandColor">{props.name}</div>
+    )
+}
