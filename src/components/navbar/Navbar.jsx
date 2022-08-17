4911dbb0d9cc7ded2ca7d0281d06858a00d2729e
iguoliveira <igor.or042@gmail.com>
2022-08-17 11:19:15 -0300

    Added logo in public + Added logo in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,16 @@
+import BlackLogo from '/cat-logo-black.png'
+
 export const Navbar = () => {
   return (
     <div className="bg-white fixed inset-0 p-3">
       <div className="flex justify-between mx-4">
-        <div className="flex gap-4">
-          <div>LOGO</div>
+        <div className="flex gap-8 text-xl">
+          <img src={BlackLogo} className="w-16" />
 
           <div className="flex items-center gap-3">
-            <div>Opa</div>
-            <div>Nao</div>
-            <div>Sim</div>
+            <div>SIM</div>
+            <div>OPA</div>
+            <div>DUDU</div>
           </div>
         </div>
 
