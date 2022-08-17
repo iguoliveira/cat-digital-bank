1820112e0eaa057815ce6869272fc30a0456fd3e
iguoliveira <igor.or042@gmail.com>
2022-08-17 16:41:09 -0300

    Homepage card done + change homepage image

---
+++ src/components/homepage/card/Card.jsx
@@ -0,0 +1,10 @@
+export const Card = () => {
+    return(
+        <div>
+            <div>Muito mais que um banco, uma nação!</div>
+            <div>
+                Rapido, seguro e agradavel!
+            </div>
+        </div>
+    )
+}
--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,25 +1,18 @@
 import { Navbar } from "../components/navbar/Navbar"
 import HomepageImg from '/woman-homepage.jpg'
-
-// export const Homepage = () => {
-//     return (
-//         <div className="h-screen bg-[url('/bg-eva-homepage.jpg')]">
-//             <Navbar />
-//             <div className="pt-20">
-//             </div>
-//         </div>
-//     )
-// }
+import { Card } from "../components/homepage/card/Card"
 
 export const Homepage = () => {
-    return(
+    return (
         <div className="h-screen">
             <Navbar />
-            <div className="pt-20">
-                <div>
-                    opa
+            <div className="pt-20 flex">
+                <div className="w-1/2 flex justify-center items-center">
+                    <Card />
                 </div>
-                <img src={HomepageImg} />
+                <div>
+                    <img src={HomepageImg} />
+                </div>
             </div>
         </div>
     )
