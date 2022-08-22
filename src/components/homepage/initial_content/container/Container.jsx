9ab52ebad54e48a253103ec6902df3160885b2fa
iguoliveira <igor.or042@gmail.com>
2022-08-21 23:30:05 -0300

    Created first component

---
+++ src/components/homepage/initial_content/container/Container.jsx
@@ -0,0 +1,9 @@
+import { Download } from "./download/Download"
+
+export const Container = () => {
+    return(
+        <div className="h-1/2 bg-mainBrandColor">
+             <Download />
+        </div>
+    )
+}
--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,9 +1,11 @@
 import { Navbar } from "../components/navbar/Navbar"
+import { Container } from "../components/homepage/initial_content/container/Container"
 
 export const Homepage = () => {
     return (
         <div className="h-screen">
             <Navbar />
+            <Container />
         </div>
     )
 }
