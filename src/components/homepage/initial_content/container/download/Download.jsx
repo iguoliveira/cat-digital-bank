c6d0a455bd53c1e74665c49b1953685652c07190
iguoliveira <igor.or042@gmail.com>
2022-08-22 08:28:04 -0300

    Started responsived

--- src/components/homepage/initial_content/container/download/Download.jsx
+++ src/components/homepage/initial_content/container/download/Download.jsx
@@ -2,7 +2,7 @@ import Pointer from "/pointer-download.png";
 
 export const Download = () => {
   return (
-    <div className="w-1/2 relative left-40 top-32 sm:">
+    <div className="w-1/2 relative left-10 top-10 md:left-20 md:top-28 xl:left-40 xl:top-32">
       <div className="text-4xl font-bold flex-wrap pb-4">
         <span className="after:content-['\a'] after:whitespace-pre">
           Your Furrvorite Bank
