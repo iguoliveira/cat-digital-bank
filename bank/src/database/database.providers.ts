cf07bc5fd9f76c8c07a99a57c0fc90b22226186e
iguoliveira <igor.or042@gmail.com>
2022-09-21 11:34:30 -0300

    👌 IMPROVE: Created User res

--- bank/src/database/database.providers.ts
+++ bank/src/database/database.providers.ts
@@ -1,13 +1,14 @@
 import { DataSource } from 'typeorm'
+import { DATA_SOURCE } from '../constants'
 
 export const databaseProviders = [
     {
-        provide: 'DATA_SOURCE',
+        provide: DATA_SOURCE,
         useFactory: async () => {
             const dataSource = new DataSource({
                 type: 'mysql',
                 host: 'localhost',
-                port: 3006,
+                port: 3306,
                 username: 'root',
                 password: 'q5J83o559T^MajnlMzIUf',
                 database: 'bank',
--- bank/src/users/users.module.ts
+++ bank/src/users/users.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
+import { userProviders } from './users.providers';
+import { UserService } from './users.service';
 
-@Module({})
+@Module({
+    imports: [DatabaseModule],
+    providers: [
+        ...userProviders,
+        UserService,
+    ],
+})
+
 export class UsersModule {}
