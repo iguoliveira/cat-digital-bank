34fb52b4dc07060170f9d87c93af5c4ec32c47d0
iguoliveira <igor.or042@gmail.com>
2022-09-22 22:06:18 -0300

    📦 NEW: Created login page

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,5 +1,64 @@
+import { useState } from "react";
+import { POST } from "../services/api";
+import { SignupValidation } from "../services/FormValidation";
+import { Link } from "react-router-dom";
+import { Notify } from "notiflix";
+import { InputText } from "../components/input/InputText";
+import { Navbar } from "../components/navbar/Navbar";
+import { Button } from "../components/input/Button";
+
 export const Login = () => {
-    return(
-        <div>LOGIN</div>
-    )
-}
+  const [cpf, setCpf] = useState("");
+  const [password, setPassword] = useState("");
+
+  var data = {
+    cpf: cpf,
+    password: password,
+  };
+
+  const SignupUser = () => {
+    event.preventDefault();
+    if (SignupValidation(cpf, password)) {
+      Notify.success("Sucesso no registro do Usuario!");
+      POST(data);
+    } else {
+      Notify.failure("Erro no registro!");
+    }
+  };
+
+  return (
+    <div className="h-screen bg-mainBrandColor-1000 overflow-hidden">
+      <Navbar />
+      <div className="h-[92%] flex flex-col justify-center items-center">
+        <div className="bg-white w-80 p-8 flex flex-col justify-center items-center rounded shadow-black shadow-md space-y-5">
+          <div className="font-bold uppercase text-2xl">Login</div>
+          <form
+            className="flex flex-col items-center space-y-4 w-72"
+            onSubmit={SignupUser}
+          >
+            <InputText
+              type="text"
+              placeholder="CPF"
+              value={cpf}
+              onchange={(evt) => setCpf(evt.target.value)}
+              maxlength="11"
+            />
+            <InputText
+              type="password"
+              placeholder="Password"
+              value={password}
+              onchange={(evt) => setPassword(evt.target.value)}
+            />
+            <Button onclick={() => SubmitEvent}>Login</Button>
+          </form>
+          <Link
+            to="/signup"
+            className="text-sm text-mainBrandColor-1000 hover:text-mainBrandColor-500"
+          >
+            Doesn't have an account? Click here and Sign Up
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/src/routes/Signup.jsx
+++ frontend/src/routes/Signup.jsx
@@ -1,33 +1,49 @@
 import { useState } from "react";
-import { POST } from '../services/api'
-import { SignupValidation } from '../services/FormValidation'
-import { Notify } from 'notiflix'
+import { POST } from "../services/api";
+import { SignupValidation } from "../services/FormValidation";
+import { Link } from "react-router-dom";
+import { Notify } from "notiflix";
 import { InputText } from "../components/input/InputText";
 import { Navbar } from "../components/navbar/Navbar";
 import { Button } from "../components/input/Button";
 
 export const Signup = () => {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [cpf, setCpf] = useState("")
-  const [birthday, setBirthday] = useState(new Date)
-  const [sex, setSex] = useState("")
-  const [password, setPassword] = useState("")
-  const [confirmpassword, setConfirmpassword] = useState("")
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [birthday, setBirthday] = useState(new Date());
+  const [sex, setSex] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmpassword] = useState("");
 
   var data = {
-    'name': name, 'sex': sex, 'email': email, 'birthDay': birthday, 'cpf': cpf, 'password': password
-  }
+    name: name,
+    sex: sex,
+    email: email,
+    birthDay: birthday,
+    cpf: cpf,
+    password: password,
+  };
 
   const SignupUser = () => {
-    event.preventDefault()
-    if(SignupValidation(name, email, cpf, password, confirmpassword, sex, birthday)){
-      Notify.success("Sucesso no registro do Usuario!")
-      POST(data)
-    }else{
-      Notify.failure('Erro no registro!')
+    event.preventDefault();
+    if (
+      SignupValidation(
+        name,
+        email,
+        cpf,
+        password,
+        confirmpassword,
+        sex,
+        birthday
+      )
+    ) {
+      Notify.success("Sucesso no registro do Usuario!");
+      POST(data);
+    } else {
+      Notify.failure("Erro no registro!");
     }
-  }
+  };
 
   return (
     <div className="h-screen bg-mainBrandColor-1000 overflow-hidden">
@@ -35,18 +51,57 @@ export const Signup = () => {
       <div className="h-[92%] flex flex-col justify-center items-center">
         <div className="bg-white w-80 p-8 flex flex-col justify-center items-center rounded shadow-black shadow-md space-y-5">
           <div className="font-bold uppercase text-2xl">Sign Up</div>
-          <form className="flex flex-col items-center space-y-4 w-72" onSubmit={SignupUser}>
-            <InputText type="text" placeholder="Name" value={name} onchange={(evt) => setName(evt.target.value)} />
-            <InputText type="email" placeholder="Email" value={email} onchange={(evt) => setEmail(evt.target.value)} />
-            <InputText type="text" placeholder="CPF" value={cpf} onchange={(evt) => setCpf(evt.target.value)} maxlength="11" />
+          <form
+            className="flex flex-col items-center space-y-4 w-72"
+            onSubmit={SignupUser}
+          >
+            <InputText
+              type="text"
+              placeholder="Name"
+              value={name}
+              onchange={(evt) => setName(evt.target.value)}
+            />
+            <InputText
+              type="email"
+              placeholder="Email"
+              value={email}
+              onchange={(evt) => setEmail(evt.target.value)}
+            />
+            <InputText
+              type="text"
+              placeholder="CPF"
+              value={cpf}
+              onchange={(evt) => setCpf(evt.target.value)}
+              maxlength="11"
+            />
             <div className="flex space-x-4">
-              <InputText type="password" placeholder="Password" value={password} onchange={(evt) => setPassword(evt.target.value)} />
-              <InputText type="password" placeholder="Confirm Password" value={confirmpassword} onchange={(evt) => setConfirmpassword(evt.target.value)} />
+              <InputText
+                type="password"
+                placeholder="Password"
+                value={password}
+                onchange={(evt) => setPassword(evt.target.value)}
+              />
+              <InputText
+                type="password"
+                placeholder="Confirm Password"
+                value={confirmpassword}
+                onchange={(evt) => setConfirmpassword(evt.target.value)}
+              />
             </div>
-            <InputText type="text" placeholder="Sex" value={sex} onchange={(evt) => setSex(evt.target.value)} />
-            <InputText type="date" value={birthday} onchange={(evt) => setBirthday(evt.target.value)} />
+            <InputText
+              type="text"
+              placeholder="Sex"
+              value={sex}
+              onchange={(evt) => setSex(evt.target.value)}
+            />
+            <InputText
+              type="date"
+              value={birthday}
+              onchange={(evt) => setBirthday(evt.target.value)}
+            />
             <Button onclick={() => SubmitEvent}>Sign up</Button>
           </form>
+          <Link to="/login" className="text-[10px] text-mainBrandColor-1000 hover:text-mainBrandColor-500">Already Signed up? Click here to Login</Link>
         </div>
       </div>
     </div>
