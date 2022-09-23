e175a1eede2f0f9f2272dc6b4eeb714238d6266d
iguoliveira <igor.or042@gmail.com>
2022-09-23 08:34:00 -0300

    👌 IMPROVE: Changed homepage route

--- frontend/src/components/homepage/Download.jsx
+++ frontend/src/components/homepage/Download.jsx
@@ -1,20 +1,40 @@
+import { useState } from "react";
 import Pointer from "/pointer-download.png";
 import { Link } from "react-router-dom";
+import { InputText } from "../input/InputText";
 
 export const Download = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
   return (
-    <div className="w-grid flex flex-col items-start">
-      <div className="text-3xl md:text-4xl font-bold flex-wrap pb-4 text-left">
-        <span className="after:content-['\a'] after:whitespace-pre uppercase text-4xl">
-          Your bank
-        </span>
-        <span>in one click</span>
+    <div className="flex flex-col items-start">
+      <div className="bg-white text-3xl md:text-4xl font-bold flex-wrap p-8 w-96 space-y-5 rounded defaultShadow">
+        <div className="uppercase text-3xl">
+          Come for the realm of benefits!
+        </div>
+        <div>
+          <InputText
+            type="text"
+            placeholder="Name"
+            value={name}
+            onchange={(evt) => setName(evt.target.value)}
+          />
+          <InputText
+            type="email"
+            placeholder="Email"
+            value={email}
+            onchange={(evt) => setEmail(evt.target.value)}
+          />
+        </div>
+        <Link
+          to="signup"
+          className="bg-mainBrandColor-1000 hover:bg-mainBrandColor-500 w-full md:w-48 text-white flex justify-center text-2xl uppercase md:text-4xl py-3 font-bold shadow-md shadow-black"
+        >
+          Sign Up
+        </Link>
       </div>
-      <Link to="signup" className="bg-black hover:bg-gray-900 w-grid md:w-48 text-white flex justify-center text-2xl uppercase md:text-4xl py-3 font-bold shadow-md shadow-black">
-        Sign Up
-      </Link>
-      <div className="relative bottom-6 md:bottom-8 left-36 md:left-36">
+      <div className="relative bottom-16 md:bottom-8 left-64 md:left-36">
         <img className="w-20 md:w-28" src={Pointer} />
       </div>
     </div>
--- frontend/src/routes/Homepage.jsx
+++ frontend/src/routes/Homepage.jsx
@@ -1,11 +1,36 @@
-import { Navbar } from "../components/navbar/Navbar"
-import { Container } from "../components/homepage/Container"
+import { Navbar } from "../components/navbar/Navbar";
+import { Download } from "../components/homepage/Download";
+import { Card } from "../components/homepage/Card";
+import DataSecurity from "/data-security.png";
+import Time from "/time.png";
+import Verification from "/verification.png";
 
 export const Homepage = () => {
-    return (
-        <div className="h-screen w-screen overflow-auto bg-mainBrandColor-1000">
-            <Navbar />
-            <Container />
+  return (
+    <div className="h-screen w-screen overflow-auto bg-mainBrandColor-1000">
+      <Navbar />
+      <div className="flex flex-col justify-between">
+        <div className="h-screen flex items-center justify-center">
+          <Download />
         </div>
-    )
-}
+        <div className="flex flex-wrap justify-around gap-24 py-32">
+          <Card
+            image={DataSecurity}
+            title="Data Security"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+          />
+          <Card
+            image={Time}
+            title="Anywhere Any Time"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+          />
+          <Card
+            image={Verification}
+            title="Certification"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
