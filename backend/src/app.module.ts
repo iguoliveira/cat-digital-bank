7abe00f27d4fa4e33a343b51d555962c1bbcd2a7
iguoliveira <igor.or042@gmail.com>
2022-09-27 11:33:20 -0300

    🐛 FIX: Removed deleted module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,13 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { CardModule } from './card/card.module';
 
 @Module({
   imports: [
     UserModule,
-    AuthModule,
-    CardModule
+    AuthModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
