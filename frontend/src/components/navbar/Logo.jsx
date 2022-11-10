095031080b9765e9117ee33565a1e6d212f8f6ec
iguoliveira <igor.or042@gmail.com>
2022-11-10 13:49:46 -0300

    📦 NEW: New frontend structure

--- frontend/src/components/navbar/Logo.jsx
+++
@@ -1,5 +0,0 @@
-import LogoNavbar from "/cat-logo-navbar.png";
-
-export const Logo = () => {
-  return <img className="w-48 md:w-56" src={LogoNavbar} />;
-};
--- frontend/src/components/navbar/link/LinkItem.jsx
+++
@@ -1,5 +0,0 @@
-export const LinkItem = (props) => {
-    return(
-        <div className="cursor-pointer font-bold text-xl hover:text-gray-700 transition">{props.name}</div>
-    )
-}
