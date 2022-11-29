93866b55436fbd50d26a15cbb00648787cb2ccb1
iguoliveira <igor.or042@gmail.com>
2022-11-29 14:59:55 -0300

    👌 IMPROVE: Mask in CPF text

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -14,6 +14,7 @@ export const Profile = () => {
     document.title = 'User Profile'
     const [user] = useUserStore((state) => [state.user])
     const [visible, isVisible] = useState(false)
+    const [isCpfVisible, setCpfVisible] = useState(false)
     const info: any = useLoaderData()
     const navigate = useNavigate()
 
@@ -50,11 +51,11 @@ export const Profile = () => {
                     </div>
                     <div className='data-box'>
                         <span className='title'>Account Number: </span>
-                        <span className='data'>{info.accountId}</span>
+                        <span className='clickable data'>{'*'.repeat(info.accountId.split("").length - 1) + info.accountId.split("").slice(-1)}</span>
                     </div>
                     <div className='data-box'>
                         <span className='title'>CPF: </span>
-                        <span className='data'>{info.cpf}</span>
+                        <span className='clickable data' onClick={() => setCpfVisible(!isCpfVisible)}>{isCpfVisible ? info.cpf.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4") : '*'.repeat(info.cpf.split('').length - 2) + '-' + info.cpf.split('')[info.cpf.split('').length - 1] + info.cpf.split('')[info.cpf.split('').length - 2]}</span>
                     </div>
                     <div className='data-box'>
                         <span className='title'>Age: </span>
@@ -70,7 +71,7 @@ export const Profile = () => {
                         data.map((item: any, index: any) => {
                             return (
                                 <div className='transaction' key={index}>
-                                    <div className='send-received'>{item.transactionType == 'pix' ? item.accountSender == user?.userAccountNumberFk ? `To ${item.accountReceiver}` : `From ${item.accountSender}` : ''}</div>
+                                    <div className='send-received'>{item.transactionType == 'pix' ? item.accountSender == user?.userAccountNumberFk ? `To ${item.accountReceiver}` : `From ${item.accountSender}` : `${user?.userAccountNumberFk}`}</div>
                                     <div className='value'>R$ {item.transactionValue}</div>
                                     <div className='verb'>{item.transactionType == 'pix' ? item.accountSender == user?.userAccountNumberFk ? (<span className='sent'>enviado</span>) : (<span className='received'>recebido</span>) : item.transactionType == 'deposit' ? (<span className='received'>depositado</span>) : (<span className='sent'>transferido</span>)}</div>
                                     <div className='type'>{item.transactionType}</div>
